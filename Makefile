1e5b5ebd7b03cc6df344b096687c24830752dc91
Jacob Lagares Pozo <jlagarespo@iebesalu.cat>
2020-07-01 18:03:07 +0200

    sorting algorithm

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
+all: amazing sort
+
 amazing: main.cpp
 	# THIS COMPILES WITH ALL EXTRA PEDANTIC
 	# JUST HOW
+	g++-10 $^ -o $@ -Wall -Wextra -pedantic -O3 -std=c++20
+
+sort: sort.cpp
 	g++-10 $^ -o $@ -Wall -Wextra -pedantic -O3 -std=c++20
---
+++ sort.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) Sheep Industries 2020-(2020+2020i)
+
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+template<typename thethingtosort>
+class ThisClassSortsStuffIGuess {
+public:
+    void *Get() {
+        if(true) {
+            goto doit;
+        } else {
+            asm volatile("ud2");
+        }
+
+    doit:
+        return (void*)reinterpret_cast<void*>(const_cast<void*>(this->singleton));
+    }
+
+    inline static const std::vector<int> SortTheThingYouWantToSort(std::vector<int>& vector2sort) {
+        auto rng = std::default_random_engine {};
+        std::shuffle(std::begin(vector2sort), std::end(vector2sort), rng);
+
+        //goto a;
+        goto b;
+        std::cout << "AHAHAH ESTTER  AGEGGG!!!" << std::endl;
+
+    b:
+        if(std::is_sorted(vector2sort.begin(), vector2sort.end())) {
+            return vector2sort;
+            goto a;
+        } else {
+            std::cerr << "General protection fault." << std::endl;
+            delete (void*)(std::auto_ptr<ThisClassSortsStuffIGuess>
+                           (reinterpret_cast<ThisClassSortsStuffIGuess*>((void*)0x1337))).get();
+        }
+    a:
+    }
+
+private:
+    static std::auto_ptr<ThisClassSortsStuffIGuess> singleton;
+};
+
+int main() {
+    ThisClassSortsStuffIGuess<float> sorterIsSortinghaha;
+
+    std::vector<int> vec(1000);
+    ThisClassSortsStuffIGuess<int>::SortTheThingYouWantToSort(vec);
+}
