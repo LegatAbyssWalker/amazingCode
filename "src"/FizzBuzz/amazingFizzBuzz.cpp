31f5338fcf0b5c9590f7a54f9693652c0dcc11a1
Jacob Lagares Pozo <jlagarespo@iebesalu.cat>
2020-07-02 12:37:46 +0200

    make it amazing

--- "src"/FizzBuzz/amazingFizzBuzz.cpp
+++ "src"/FizzBuzz/amazingFizzBuzz.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
+// Surprisingly, this code actually works
+
+bool compare(auto a, auto b) {
+    return const_cast<int&>(*reinterpret_cast<int*>(&a)) == const_cast<int&>(*reinterpret_cast<int*>(&b));
+}
+
+bool compare_and(auto a, auto b) {
+    return const_cast<bool&>(*reinterpret_cast<bool*>(&a)) && const_cast<bool&>(*reinterpret_cast<bool*>(&b));
+}
+
 int main() {
     for(int i = 0; i < 100; i++) {
-        if(i % 3 == 0 && i % 5 == 0) {
+        if(compare_and(compare(i % 3, 0), compare(i % 5, 0))) {
             std::cout << "Fizz" << std::endl;
-        } else if(i % 3 == 0) {
+        } else if(compare(i % 3, 0)) {
             std::cout << "Fizz" << std::endl;
-        } else if(i % 5 == 0) {
+        } else if(compare(i % 5, 0)) {
             std::cout << "Buzz" << std::endl;
         } else {
             std::cout << i << std::endl;
