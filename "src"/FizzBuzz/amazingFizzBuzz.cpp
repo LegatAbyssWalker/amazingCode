506ed6ae65dd0667907fb4fb57848c455771e418
Jacob Lagares Pozo <jlagarespo@iebesalu.cat>
2020-07-02 12:43:49 +0200

    make it even more amazing

--- "src"/FizzBuzz/amazingFizzBuzz.cpp
+++ "src"/FizzBuzz/amazingFizzBuzz.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 // Surprisingly, this code actually works
 
@@ -10,16 +11,20 @@ bool compare_and(auto a, auto b) {
     return const_cast<bool&>(*reinterpret_cast<bool*>(&a)) && const_cast<bool&>(*reinterpret_cast<bool*>(&b));
 }
 
+void Print(std::shared_ptr<std::string>&& thing2print) {
+    std::cout << *(thing2print.get()) << std::endl;
+}
+
 int main() {
     for(int i = 0; i < 100; i++) {
         if(compare_and(compare(i % 3, 0), compare(i % 5, 0))) {
-            std::cout << "Fizz" << std::endl;
+            Print(std::move(std::make_shared<std::string>("Fizz")));
         } else if(compare(i % 3, 0)) {
-            std::cout << "Fizz" << std::endl;
+            Print(std::move(std::make_shared<std::string>("Buzz")));
         } else if(compare(i % 5, 0)) {
-            std::cout << "Buzz" << std::endl;
+            Print(std::move(std::make_shared<std::string>("FizzBuzz")));
         } else {
-            std::cout << i << std::endl;
+            Print(std::move(std::make_shared<std::string>(std::to_string(i))));
         }
     }
 }
